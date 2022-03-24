a8f83ef3a24ebe4dfc4053f103f9f6fa775b0d28
NazrulChowdhury <nazrulchowdhury404@gmail.com>
2022-03-24 17:19:59 +0000

    form validation added

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,19 +1,39 @@
 import React, { useContext, useState, useEffect } from 'react'
 import styles from '../Style.scss'
 import { ActionContext } from './ActionContext'
+import { useForm } from "react-hook-form"
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from "yup"
+
+
+const schema = yup.object({
+  comment: yup.string()
+  .min(1, 'cannot submit empty!')
+  .max(30,'maximum 1000 characters allowed! Try creating a thread instead.')
+  .required()
+})
 
 const InputField = ({ cancellor, parentId, child, value, edit, main, fullName, targetUserId, targetCommentId,replyId }) => {
-  const [text, setText] = useState(!parentId? '' : `@${fullName}   `)
-  const handleChange = (e) => {
-    setText(e.target.value) 
+ 
+  const [text, setText] = useState('')
+  const { register, handleSubmit, formState:{ errors } } = useForm({
+    resolver: yupResolver(schema),
+    defaultValues : {"comment" : !parentId? '' : `@${fullName}   `}
+  })
+  const postForm = (data) => {
+    edit === true
+    ? actions.submit(cancellor, data.comment, parentId, true, setText, null, targetCommentId)
+    : actions.submit(cancellor, data.comment, parentId, false, setText,targetUserId, targetCommentId)
   }
-
-  useEffect(() => {
+  const onSubmit = (data) => postForm(data)
+ 
+  useEffect(() => { 
     setText(value)
-  }, [value])
+  }, [text])
   
   const actions = useContext(ActionContext)
   return (
+    <div>
     <form
       className={styles.form}
       style={
@@ -29,24 +49,25 @@ const InputField = ({ cancellor, parentId, child, value, edit, main, fullName, t
           alt='userIcon'
         />
       </div>
-      <input
+      <input 
+        {...register("comment")} 
         className={styles.postComment}
-        type='text'
         placeholder='Type your reply here.'
-        component='input'
+      />
+      {/* <textarea
+        className={styles.postComment}
+        //type='text'
+        placeholder='Type your reply here.'
+        //component='input'
         value={text} 
         onChange={handleChange}
-      />
+      /> */}
       <div className={styles.inputActions}>
         <button
           className={styles.postBtn}
-          onClick={() =>
-            edit === true
-              ? actions.submit(cancellor, text, parentId, true, setText, null, targetCommentId)
-              : actions.submit(cancellor, text, parentId, false, setText,targetUserId, targetCommentId)
-          }
+          onClick={handleSubmit(onSubmit)}
           type='button'
-          disabled={!text}
+          // disabled={!text}
           style={
             !text
               ? { backgroundColor: '#84dcff' }
@@ -69,7 +90,83 @@ const InputField = ({ cancellor, parentId, child, value, edit, main, fullName, t
         )}
       </div>
     </form>
+    <p style={{marginLeft : '20px', color : 'red'}}>{errors.comment?.message}</p>
+    </div>
   )
 }
 
 export default InputField
+
+
+
+// const InputField = ({ cancellor, parentId, child, value, edit, main, fullName, targetUserId, targetCommentId,replyId }) => {
+//   const [text, setText] = useState(!parentId? '' : `@${fullName}   `)
+//   const handleChange = (e) => {
+//     setText(e.target.value) 
+//   }
+
+//   useEffect(() => { 
+//     setText(value)
+//   }, [value])
+  
+//   const actions = useContext(ActionContext)
+//   return (
+//     <form
+//       className={styles.form}
+//       style={
+//         !child && !edit && main === undefined
+//           ? { marginLeft: 36 }
+//           : { marginLeft: 8 }
+//       }
+//     >
+//       <div className={styles.userImg}>
+//         <img
+//           src={actions.userImg}
+//           style={{ width: 38, height: 38, borderRadius: 38 / 2 }}
+//           alt='userIcon'
+//         />
+//       </div>
+//       <textarea
+//         className={styles.postComment}
+//         //type='text'
+//         placeholder='Type your reply here.'
+//         //component='input'
+//         value={text} 
+//         onChange={handleChange}
+//       />
+//       <div className={styles.inputActions}>
+//         <button
+//           className={styles.postBtn}
+//           onClick={() =>
+//             edit === true
+//               ? actions.submit(cancellor, text, parentId, true, setText, null, targetCommentId)
+//               : actions.submit(cancellor, text, parentId, false, setText,targetUserId, targetCommentId)
+//           }
+//           type='button'
+//           disabled={!text}
+//           style={
+//             !text
+//               ? { backgroundColor: '#84dcff' }
+//               : { backgroundColor: '#30c3fd' }
+//           }
+//         >
+//           Post
+//         </button>
+//         {(text || parentId) && (
+//           <button
+//             className={styles.cancelBtn}
+//             onClick={() =>
+//               edit
+//                 ? actions.handleCancel(cancellor, edit)
+//                 : actions.handleCancel(cancellor)
+//             }
+//           >
+//             Cancel
+//           </button>
+//         )}
+//       </div>
+//     </form>
+//   )
+// }
+
+// export default InputField
